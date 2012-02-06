4029a89f0e7ee49a4640a11b75ad85d548d5507c
Andy Petrella <andy.petrella@gmail.com>
2012-02-07 00:37:28 +0100

    update for sbt-idea

--- project/Build.scala
+++ project/Build.scala
@@ -7,12 +7,16 @@ object ApplicationBuild extends Build {
     val appName         = "Play20WithNeo4J"
     val appVersion      = "1.0"
 
+	val sbtIdeaRepo = "sbt-idea-repo" at "http://mpeltonen.github.com/maven/"
+	  
     val appDependencies = Seq(
       // Add your project dependencies here,
     )
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
-      // Add your own project settings here      
+      resolvers ++= Seq(
+        sbtIdeaRepo
+      )
     )
 
 }
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,5 +1,3 @@
-logLevel := Level.Warn
-
 resolvers ++= Seq(
     DefaultMavenRepository,
     Resolver.url("Play", url("http://download.playframework.org/ivy-releases/"))(Resolver.ivyStylePatterns),
@@ -7,3 +5,9 @@ resolvers ++= Seq(
 )
 
 addSbtPlugin("play" % "sbt-plugin" % "2.0-RC1-SNAPSHOT")
+
+resolvers += "sbt-idea-repo" at "http://mpeltonen.github.com/maven/"
+
+addSbtPlugin("com.github.mpeltonen" % "sbt-idea" % "0.11.1-SNAPSHOT")
+
+libraryDependencies += "play" %% "play" % "2.0-RC1-SNAPSHOT"
